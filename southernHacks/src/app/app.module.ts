f4665013be910b5d301fc0cdc704b4b4af1ac984
unknown <sb09745@georgiasouthern.edu>
2019-07-21 21:31:22 -0600

    added all of the components and am starting the intro section

--- southernHacks/src/app/app.module.ts
+++ southernHacks/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { IntroComponent } from './intro/intro.component';
+import { RegisterComponent } from './register/register.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScheduleComponent,
+    IntroComponent,
+    RegisterComponent,
+    SponsorsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
